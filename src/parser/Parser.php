6aa1de70a94ae4971d3497b527349432be3274e8
andrei_doroteo <andreitot13@gmail.com>
2025-10-08 11:14:06 -0700

    feat(Parser.php): Add specification for parse method.

--- src/parser/Parser.php
+++ src/parser/Parser.php
@@ -2,21 +2,32 @@
 
 namespace DoroteoDigital\AutoEmail\parser;
 
-class Parser {
+class Parser
+{
 
 
-	/*
-	 *
-	 */
-	function __construct() {
+    /*
+     *
+     */
+    function __construct()
+    {
 
-	}
+    }
 
-	/*
-	 *
-	 */
-	public function parse() {
-
-	}
+    /*
+     * Goes through $file line by line and replaces anything in the form {{template_variable}}
+     * with respective value in $vars if any.
+     * Template variable replacing starts after first curly bracket and ends after at last curly bracket.
+     * Any whitespace inside the first and last curly brackets are ignored. Template variable names cannot have spaces.
+     * if there is no value to replace with in $vars, throws MissingTemplateVariableException.
+     */
+    public function parse(string $file, array $vars): string
+    {
+        // TODO
+        /**
+         * Draft implementation: preg_replace('/{\s?{\s?\w*\s?}\s?}/', "Replacement", $file)
+         */
+        return ""; // stub
+    }
 
 }
