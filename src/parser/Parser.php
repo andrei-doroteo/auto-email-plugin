b9d4926b1e5fcf881963ba7849366ff0b5cdb31e
andrei_doroteo <andreitot13@gmail.com>
2025-10-20 19:53:16 -0700

    update(Parser.php): Update specification for parse method.
    MERGE WITH LAST COMMIT

--- src/parser/Parser.php
+++ src/parser/Parser.php
@@ -14,14 +14,33 @@ class Parser
 
     }
 
-    /*
-     * Goes through $file line by line and replaces anything in the form {{template_variable}}
-     * with respective value in $vars if any.
-     * Template variable replacing starts after first curly bracket and ends after at last curly bracket.
-     * Any whitespace inside the first and last curly brackets are ignored. Template variable names cannot have spaces.
-     * if there is no value to replace with in $vars, throws MissingTemplateVariableException.
+    /**
+     * Parses template variables in $file and replaces them with provided values in $vars.
+     * 
+     * Matches template variables in the form {{variable_name}}, in $file and replaces each
+     * with the corresponding value from $vars if any.
+     *
+     * A template variable begins at the first opening curly bracket and ends after the second closing curly bracket.
+     * Any whitespace within the first and last curly brackets are ignored. Template variable names cannot have spaces
+     * and must be at least one character long.
+     * 
+     * If there is no key in $vars with a value to replace the template variable, that unset key is appended to $unset
+     * and the template variable in $file is unchanged.
+     * 
+     * Template Variable Examples:
+     *  - {{my_name}}
+     *  - {{ my_name }}
+     *  - { {my_name} }
+     *  - { { my_name } }
+     *  - {{ my_name} }
+     *
+     * @param string $file The string that gets replaced.
+     * @param array $vars An associative array of template variable names and their values for replacement.
+     * @param array $unset An optional array, passed by reference, that will be overwritten and populated with all unset vars.
+     *
+     * @return string The parsed version of $file.
      */
-    public function parse(string $file, array $vars): string
+    public function parse(string $file, array $vars, array &$unset = []): string
     {
         // TODO
         /**
