20acd06d4c19cec34a0e16674412746b14cd3478
andrei_doroteo <andreitot13@gmail.com>
2025-11-27 12:04:52 -0800

    feat(Sender.php): Specify Sender class.

--- src/sender/Sender.php
+++ src/sender/Sender.php
@@ -6,15 +6,33 @@ class Sender {
 
 
 	/*
-	 *
+	 * Initializes a Sender object
 	 */
 	function __construct() {
 	}
 
 	/*
+	 * Sends an email with the given $message
 	 *
+	 * This email is sent to the address in $to, with
+	 * the given subject and headers.
 	 */
-	public function send() {
+	public function send(
+		string $to,
+		string $subject,
+		string $message,
+		string $headers = '',
+		$attachments = array()
+	) {
 
+		//TODO
+
+		// Example Data
+//		$to      = 'sendto@example.com';
+//		$subject = 'The subject';
+//		$message    = 'The email body content';
+//		$headers = array( 'Content-Type: text/html; charset=UTF-8' );
+
+		wp_mail( $to, $subject, $message, $headers ); // Stub
 	}
 }
