a09911e2d4967b202a569cc61b068063aa894fd8
andrei_doroteo <andreitot13@gmail.com>
2026-01-27 23:44:04 -0800

    feat(index.tsx): Add tanstack-query QueryProvider to root component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
-import "./frontend/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import domReady from "@wordpress/dom-ready";
 import { createRoot } from "@wordpress/element";
+import "./frontend/index.css";
 import { SettingsPage } from "./frontend/SettingsPage";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 domReady(() => {
-	const settingsMenu = document.getElementById("auto-email-settings-menu");
-	if (settingsMenu) {
-		const root = createRoot(settingsMenu);
-		root.render(<SettingsPage />);
-	}
+    const settingsMenu = document.getElementById("auto-email-settings-menu");
+    if (settingsMenu) {
+        const root = createRoot(settingsMenu);
+        root.render(
+            <QueryClientProvider client={queryClient}>
+                <SettingsPage />
+            </QueryClientProvider>
+        );
+    }
 });
